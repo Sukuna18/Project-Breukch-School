c82c52d858ec0e6f76a730360d1b747babb70ffc
Sukuna18 <gossfall10@gmail.com>
2023-05-17 23:25:35 +0000

    some style modification

--- views/StudentList.php
+++ views/StudentList.php
@@ -1,15 +1,100 @@
 <div class="container-fluide">
+		<!-- Navbar -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <!-- Container wrapper -->
+  <div class="container">
+    <!-- Navbar brand -->
+    <a class="navbar-brand" href="#"><img id="MDB-logo"
+        src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/modern-school-logo-design-template-1d88683e857f70116bf3ba828be9a84e_screen.jpg?ts=1576966343" alt="MDB Logo"
+        draggable="false" height="30" /></a>
+
+    <!-- Toggle button -->
+    <button class="navbar-toggler" type="button" data-mdb-toggle="collapse"
+      data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+      aria-label="Toggle navigation">
+      <i class="fas fa-bars"></i>
+    </button>
+
+    <!-- Collapsible wrapper -->
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <!-- Left links -->
+      <ul class="navbar-nav me-3">
+        <li class="nav-item">
+          <a class="nav-link active d-flex align-items-center" aria-current="page" href="#"><i
+              class="fas fa-bars pe-2"></i>Menu</a>
+        </li>
+      </ul>
+      <!-- Left links -->
+
+      <form class="d-flex align-items-center w-100 form-search">
+        <div class="input-group">
+          <button class="btn btn-light dropdown-toggle shadow-0" type="button" data-mdb-toggle="dropdown"
+            aria-expanded="false" style="padding-bottom: 0.4rem;">
+            All
+          </button>
+          <ul class="dropdown-menu dropdown-menu-dark fa-ul">
+            <li>
+              <a class="dropdown-item" href="#"><span class="fa-li pe-2"><i class="fas fa-search"></i></span>All</a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="#"><span class="fa-li pe-2"><i
+                    class="fas fa-film"></i></span>Titles</a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="#"><span class="fa-li pe-2"><i class="fas fa-tv"></i></span>TV
+                Episodes</a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="#"><span class="fa-li pe-2"><i
+                    class="fas fa-user-friends"></i></span>Celebs</a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="#"><span class="fa-li pe-2"><i
+                    class="fas fa-building"></i></span>Companies</a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="#"><span class="fa-li pe-2"><i
+                    class="fas fa-key"></i></span>Keywords</a>
+            </li>
+            <li>
+              <hr class="dropdown-divider" />
+            </li>
+            <li>
+              <a class="dropdown-item" href="#"><span class="fa-li pe-2"><i
+                    class="fas fa-search-plus"></i></span>Advanced
+                search<i class="fas fa-chevron-right ps-2"></i></a>
+            </li>
+          </ul>
+          <input type="search" class="form-control" placeholder="Search" aria-label="Search" />
+        </div>
+        <a href="#!" class="text-white"><i class="fas fa-search ps-3"></i></a>
+      </form>
+
+      <ul class="navbar-nav ms-3">
+        <li class="nav-item">
+          <a class="nav-link d-flex align-items-center me-3" href="#!">
+            <i class="fas fa-bookmark pe-2"></i> Liste
+          </a>
+        </li>
+      </ul>
+    </div>
+    <!-- Collapsible wrapper -->
+  </div>
+  <!-- Container wrapper -->
+</nav>
+<!-- Navbar -->
+
 <div class="row">
 	<div class="col-lg-12">
 		<div class="main-box clearfix">
 			<div class="table-responsive">
 				<table class="table user-list">
 					<thead>
-						<tr>
-							<th><span>Utilisateur</span></th>
-							<th><span>Date de creation</span></th>
+						<tr style="background-color: white; font-size:18px;">
+							<th><span>Eleve</span></th>
+							<th><span>Annee Scolaire</span></th>
 							<th class="text-center"><span>Status</span></th>
-							<th><span>Classe</span></th>
+							<th><span>Niveau</span></th>
 							<th>&nbsp;</th>
 						</tr>
 					</thead>
@@ -20,7 +105,7 @@
 								<a href="#" class="user-link">Bamba Fall</a>
 							</td>
 							<td>
-								2023/05/19
+								2022-2023
 							</td>
 							<td class="text-center">
 								<span class="label label-default">Inactif</span>
@@ -55,7 +140,7 @@
 								<a href="#" class="user-link">Sukuna Ryomen</a>
 							</td>
 							<td>
-								2023/05/19
+							2022-2023
 							</td>
 							<td class="text-center">
 								<span class="label label-success">Actif</span>
@@ -90,7 +175,7 @@
 								<a href="#" class="user-link">Goss Fall</a>
 							</td>
 							<td>
-                            2023/05/19
+                            2022-2023
 							</td>
 							<td class="text-center">
 								<span class="label label-danger">Actif</span>
@@ -125,7 +210,7 @@
 								<a href="#" class="user-link">Goss Valadaresse</a>
 							</td>
 							<td>
-                            2023/05/19
+                            2022-2023
 							</td>
 							<td class="text-center">
 								<span class="label label-warning">Inactif</span>
@@ -160,7 +245,7 @@
 								<a href="#" class="user-link">Atsuchi Murasakibara</a>
 							</td>
 							<td>
-                            2023/05/19
+                            2022-2023
 							</td>
 							<td class="text-center">
 								<span class="label label-success">Actif</span>
@@ -195,7 +280,7 @@
 								<a href="#" class="user-link">Daiki Aomine</a>
 							</td>
 							<td>
-                            2023/05/19
+                            2022-2023
 							</td>
 							<td class="text-center">
 								<span class="label label-default">Inactif</span>
@@ -230,7 +315,7 @@
 								<a href="#" class="user-link">Ryota Kise</a>
 							</td>
 							<td>
-                            2023/05/19
+                            2022-2023
 							</td>
 							<td class="text-center">
 								<span class="label label-warning">Actif</span>
@@ -265,7 +350,7 @@
 								<a href="#" class="user-link">Tetsuya Kuroko</a>
 							</td>
 							<td>
-                            2023/05/19
+                            2022-2023
 							</td>
 							<td class="text-center">
 								<span class="label label-success">Actif</span>
